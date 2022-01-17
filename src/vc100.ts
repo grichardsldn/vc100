5ee0be2f8d683071afac52a31f1b1a13fda2c3ed
Graham Richards <grahamdrichards@gmail.com>
2022-01-17 08:31:14 +0000

    socketio for background colour

--- src/vc100.ts
+++ src/vc100.ts
@@ -1,11 +1,31 @@
 import express from 'express';
 
 const app = express()
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-app.get("/", async (req: express.Request, res: express.Response) => {
-  console.log("call to / with query: " + JSON.stringify( req.query) )
+io.on('connection', (socket: any) => {
+  console.log('a user connected');
+  io.emit('message', 'hi');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
 
-  res.send('hello world')
+
+app.get("/background/:colour", async (req: express.Request, res: express.Response) => {
+  //console.log("call to / with query: " + JSON.stringify( req.query) )
+  console.log(`background: ${req.params.colour}`)
+  io.emit('background', req.params.colour)
+  res.send('ok')
 })
 
-app.listen(3000)
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
